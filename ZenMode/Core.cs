cd843e9698dd3f347713ee7e4aba75d02d6d3a46
Libella <libellav2@gmail.com>
2024-09-25 22:01:11 -0400

    Add project files.

---
+++ ZenMode/Core.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(ZenMode.Core), "ZenMode", "1.0.0", "valer", null)]
+[assembly: MelonGame("Little Flag Software, LLC", "Neon White")]
+
+namespace ZenMode
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            //Hi :3
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
